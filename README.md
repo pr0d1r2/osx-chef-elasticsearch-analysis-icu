05a60ef7bae1b05795e3249d2b63ff40c93b449e
Marcin Nowicki <pr0d1r2@gmail.com>
2016-03-08 05:49:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# osx-chef-elasticsearch-analysis-icu
+Install ICU Analysis plugin for Elasticsearch for OSX machines (to be used in Kitchenplan)
---
+++ recipes/default.rb
@@ -0,0 +1,14 @@
+elasticsearch_version = `/usr/local/bin/elasticsearch -v | cut -f 1 -d , | cut -f 2 -d : | cut -f1-2 -d .`.strip
+
+case elasticsearch_version
+when '1.7'
+  elasticsearch_analysis_icu_version = '2.7.0'
+when '1.5'
+  elasticsearch_analysis_icu_version = '2.6.0'
+else
+  raise "Unsupported elasticsearch version: #{elasticsearch_version}"
+end
+
+execute "/usr/local/bin/plugin -install elasticsearch/elasticsearch-analysis-icu/#{elasticsearch_analysis_icu_version}" do
+  not_if { File.exist?("/usr/local/var/lib/elasticsearch/plugins/analysis-icu/elasticsearch-analysis-icu-#{elasticsearch_analysis_icu_version}.jar") }
+end
